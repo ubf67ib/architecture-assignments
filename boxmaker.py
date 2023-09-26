bcdacb452e0fa7258e3272802736997c1506ce6e
ubf67ib <146124620+ubf67ib@users.noreply.github.com>
2023-09-26 14:14:24 -0700

    using loops to condense 12 lines of code into 20 lines ;) (and actually adding curves & comments)

--- boxmaker.py
+++ boxmaker.py
@@ -1,5 +1,6 @@
 import rhinoscriptsyntax as rs
 
+#I don't want to type out this massive line ever again so I'm making a function
 def makeBox(x, y, z, l, w, h):
     rs.AddBox([[x, y+w, z], [x, y, z], [x+l, y, z], [x+l, y+w, z], [x, y+w, z+h], [x, y, z+h], [x+l, y, z+h], [x+l, y+w, z+h]])
 
--- cageBox.py
+++ cageBox.py
@@ -1,17 +1,21 @@
 from boxmaker import *
 
+#It's a function because reasons
 def makeCagebox():
     x = y = z = 0
     l = w = h = 10
     lines  = []
     i = 0
+
+    #Layers were a part of the assignment
     outsideBoxes = rs.createColor(124, 150, 255)
     rs.addLayer("BOXES", outsideBoxes)
     rs.currentLayer("BOXES")
     rs.deleteLayer(Default)
-    lines = rs.createColor(106, 255, 158)
-    rs.addLayer("CURVES", lines)
+    lineColor = rs.createColor(106, 255, 158)
+    rs.addLayer("CURVES", lineColor)
 
+    #Create 8 outer boxes
     while z <= 40:
         while x <= 40:
             while y <= 40:
@@ -23,8 +27,37 @@ def makeCagebox():
         x = 0
         y = 0
 
+    #Now create the 12 lines in between the boxes
     rs.currentlayer("CURVES")
     x = y = z = 10
     l = w = h = 30
 
-makeCagebox()
+    #Using loops to condense 12 lines of code into 20 lines ;)
+    while z <= 40:
+        while y <= 40:
+            lines[i] = rs.newLine([[x, y, z], [x+l, y, z]])
+            i =+ 1
+            y += 30
+        y = 0
+        z += 30
+    x = y = z = 0
+
+    while z <= 40:
+        while x <= 40:
+            lines[i] = rs.newLine([[x, y, z], [x, y+w, z]])
+            i =+ 1
+            x += 30
+        x = 0
+        z += 30
+    x = y = z = 0
+
+    while x <= 40:
+        while y <= 40:
+            lines[i] = rs.newLine([[x, y, z], [x, y, z+h]])
+            i =+ 1
+            y += 30
+        y = 0
+        x += 30
+
+if __name__ == "__main__":
+    makeCagebox()
