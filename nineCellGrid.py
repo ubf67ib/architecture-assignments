8ffc66c24d1367d3e63109d77af53eae7b66489e
ubf67ib <146124620+ubf67ib@users.noreply.github.com>
2023-11-27 14:21:38 -0800

    maybe this is how planes work?

--- nineCellGrid.py
+++ nineCellGrid.py
@@ -3,7 +3,7 @@ from boxmaker import *
 
 #There's no split by plane, so this function will be useful later
 def planeToBrep(plane):
-    
+
 
 #I can't be bothered to make it object oriented
 def makeBox():
@@ -23,9 +23,12 @@ def makeBox():
     for i in lines:
         points[i] = rs.DivideCurve(lines[i], 18, create_points=True, return_points=True)
     
+    #TODO: make this user imputted
     splitNumbers = [[6, 12], [4, 10], [8, 14]]
     for i in splitNumbers[0]:
-        planes[0][i] = 
+        planes[0][i] = rs.PlaneFromNormal(lines[0][i], [1, 0, 0])
+    for i in splitNumbers[1]
+        planes[0][i] = rs.PlaneFromNormal(lines[0][i], [0, 1, 0])
 
 if __name__ == "__main__":
     makeBox()
