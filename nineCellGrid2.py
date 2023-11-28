5a09e1fd21030310c7a4ebb3442bc2419177cf41
ubf67ib <146124620+ubf67ib@users.noreply.github.com>
2023-11-28 10:08:55 -0800

    more typo

--- nineCellGrid2.py
+++ nineCellGrid2.py
@@ -6,8 +6,8 @@ lines = [[0.3, 0.6], [0.4, 0.8], [0.2, 0.7]]
 x, y, z = 0, 0, 0
 
 for i in lines[0]:
-    makebox(x+i*9-3/32, y, z, 3/16, 9, 9)
+    makeBox(x+i*9-3/32, y, z, 3/16, 9, 9)
 for i in lines[1]:
-    makebox(x, y+i*9-3/32, z, 9, 3/16, 9)
+    makeBox(x, y+i*9-3/32, z, 9, 3/16, 9)
 for i in lines[2]:
-    makebox(x, y, z+i*9-3/32, 9, 9, 3/16)
+    makeBox(x, y, z+i*9-3/32, 9, 9, 3/16)
