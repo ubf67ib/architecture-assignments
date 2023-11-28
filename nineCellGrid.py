3288ff8b2cbd9fd46b3d01cbc84490650eacfabd
ubf67ib <146124620+ubf67ib@users.noreply.github.com>
2023-11-28 10:21:15 -0800

    Documentation

--- nineCellGrid.py
+++ nineCellGrid.py
@@ -5,6 +5,10 @@ from boxmaker import *
 lines = [[0.3, 0.6], [0.4, 0.8], [0.2, 0.7]]
 x, y, z = 0, 0, 0
 
+#Puteverything on a new layer
+rs.CurrentLayer = rs.AddLayer()
+
+#Just make the boxes
 for i in lines[0]:
     makeBox(x+i*9-3/32, y, z, 3/16, 9, 9)
 for i in lines[1]:
