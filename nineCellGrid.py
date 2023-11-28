b41101a9a46abb6002dfc45b16b35e49d08f69f3
ubf67ib <146124620+ubf67ib@users.noreply.github.com>
2023-11-28 08:21:28 -0800

    dicts

--- nineCellGrid.py
+++ nineCellGrid.py
@@ -16,19 +16,32 @@ def makeBox():
         rs.CurrentLayer(layer)
 
     makeBox(x, y, z, l, w, h)
+    lines = {}
     lines[0] = rs.NewLine((x, y, z+h), (x+l, y, z+h))
     lines[1] = rs.NewLine((x+l, y, z+h), (x+l, y+w, z+h))
     lines[2] = rs.NewLine((x+l, y, z+h), (x+l, y, z))
 
+    points = {}
     for i in lines:
         points[i] = rs.DivideCurve(lines[i], 18, create_points=True, return_points=True)
     
     #TODO: make this user imputted
     splitNumbers = [[6, 12], [4, 10], [8, 14]]
+
+    pl
     for i in splitNumbers[0]:
         planes[0][i] = rs.PlaneFromNormal(lines[0][i], [1, 0, 0])
-    for i in splitNumbers[1]
-        planes[0][i] = rs.PlaneFromNormal(lines[0][i], [0, 1, 0])
+    for i in splitNumbers[1]:
+        planes[1][i] = rs.PlaneFromNormal(lines[1][i], [0, 1, 0])
+    for i in splitNumbers[2]:
+        planes[2][i] = rs.PlaneFromNormal(lines[2][i], [0, 0, 1])
+
+    #Now make the planes into breps because you can't split by plane
+    i = 0
+    while i < len(planes):
+        for j in planes[i]:
+            planeSrfs[i][]
+        i += 1
 
 if __name__ == "__main__":
     makeBox()
